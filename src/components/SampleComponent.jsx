ddb340161522ddb0c1d37544d27680eba49e1cdf
kkrriiss <powellk@utas.edu.au>
2016-11-25 16:30:00 +1100

    Component and Reducer Hot Reloading

--- src/components/SampleComponent.jsx
+++ src/components/SampleComponent.jsx
@@ -2,19 +2,20 @@ import React, {Component} from "react";
 import {connect} from "react-redux";
 
 const mapState = state => ({
-    data : state.test.data
+  data : state.test.data
 });
 
 class SampleComponent extends Component {
-    render() {
-        const {data} = this.props;
+  render() {
+    // throw new Error("Ooop");
+    const {data} = this.props;
 
-        return (
-            <div>
-                Data from Redux: {data}
-            </div>
-        );
-    }
+    return (
+      <div>
+        Data from Redux: {data}
+      </div>
+    );
+  }
 }
 
 export default connect(mapState)(SampleComponent);
